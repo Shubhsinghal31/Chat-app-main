8275c9afbe68436441784495fb9546840b159c8d
Shubhsinghal31 <sj.singhal01@gmail.com>
2025-03-08 15:04:02 +0530

    first commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
+  base: '/',
+  plugins: [react()],
+  server: {
+    proxy: process.env.NODE_ENV === 'development' ? {
+      '/api': 'http://localhost:3000',
+    } : {},
+  },
+});
